935fbd4ccbfae5334b3f70a22a49f81cc621e8f6
Martin Georgiev <martin.geor04@gmail.com>
2024-09-28 01:17:57 +0300

    Added navigation component

---
+++ MSMS.Web/Components/NavigationComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSMS.Web.Components
+{
+    public class NavigationComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
