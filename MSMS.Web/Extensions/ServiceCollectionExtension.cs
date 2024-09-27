0b70ed6e082df60459627bfe21f239a23f69f369
Martin Georgiev <martin.geor04@gmail.com>
2024-09-28 01:05:01 +0300

    Created extensions for service collection. Changed identity user to application user

---
+++ MSMS.Web/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MSMS.Infrastructure.Data;
+using MSMS.Infrastructure.Data.Models;
+
+namespace MSMS.Web.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddDbContext(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MSMSConnectionString");
+            serviceCollection.AddDbContext<MSMSDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddIdentity(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<MSMSDbContext>();
+
+            return serviceCollection;
+        }
+    }
+}
