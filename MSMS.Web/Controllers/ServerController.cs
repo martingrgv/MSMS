e9bfd55f4cbe8b34c4a3cca1ded5f3b23f7b4356
Martin Georgiev <martin.geor04@gmail.com>
2024-09-29 22:39:17 +0300

    Created server controller and view

---
+++ MSMS.Web/Controllers/ServerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSMS.Web.Controllers
+{
+    public class ServerController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
