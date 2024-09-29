a7033559457b7ba169a161d987de2d3bde2974b8
Martin Georgiev <martin.geor04@gmail.com>
2024-09-30 00:10:56 +0300

    Added details and create pages

--- MSMS.Web/Controllers/ServerController.cs
+++ MSMS.Web/Controllers/ServerController.cs
@@ -4,7 +4,20 @@ namespace MSMS.Web.Controllers
 {
     public class ServerController : Controller
     {
+        [HttpGet]
         public IActionResult All()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details([FromRoute] int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
