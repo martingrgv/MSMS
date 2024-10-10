016e04e1f8a8db9f92f761f18cc18c42e9dfb573
Martin Georgiev <martin.geor04@gmail.com>
2024-10-10 22:51:38 +0300

    Server-Operations: added server all and create methods, attach server image, update models and views, moved repository

---
+++ MSMS.Core/Models/ServerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MSMS.Infrastructure.Data.Enums;
+
+namespace MSMS.Core.Models
+{
+    public class ServerViewModel
+    {
+        public string Name { get; set; } = null!;
+        public string ImagePath { get; set; } = null!;
+        public string IpAddress { get; set; } = null!;
+        public string GameVersion { get; set; } = null!;
+        public PlayMode PlayMode { get; set; }
+        public string? Description { get; set; } = null!;
+        public string OwnerName { get; set; } = null!;
+    }
+}
---
+++ MSMS.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MSMS.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        ICollection<TEntity> All<TEntity>() where TEntity : class;
+        IEnumerable<TEntity> AllReadOnly<TEntity>() where TEntity : class;
+        void Add<TEntity>(TEntity entity) where TEntity : class;
+        void AddRange<TEntity>(List<TEntity> entities) where TEntity : class;
+        TEntity? GetById<TEntity>(int id) where TEntity : class;
+        void Remove<TEntity>(TEntity entity) where TEntity : class;
+        void RemoveRange<TEntity>(ICollection<TEntity> entities) where TEntity : class;
+        int SaveChanges();
+    }
+}
