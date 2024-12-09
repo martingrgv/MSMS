577c6d45e1e80d2c793726086532bbd63253c4cd
martin.grgv <martin.geor04@gmail.com>
2024-12-10 01:25:33 +0200

    Portal-Tool: added portal tool logic: JS only

---
+++ MSMS.Web/Controllers/PortalToolController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MSMS.Web.Controllers
+{
+    public class PortalToolController : Controller
+    {
+        public ActionResult Tool()
+        {
+            return View();
+        }
+
+    }
+}
